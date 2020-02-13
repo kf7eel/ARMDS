0c89ab19482799b60f1b67688534900e842ac897
KF7EEL <57960556+kf7eel@users.noreply.github.com>
2020-02-12 18:33:44 -0800

    Update copyright

--- core.py
+++ core.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.7
 
 ###############################################################################
-#   Copyright (C) 2019 Eric Craw, KF7EEL <kf7eel@qsl.net>
+#   Copyright (C) 2020 Eric Craw, KF7EEL <kf7eel@qsl.net>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
